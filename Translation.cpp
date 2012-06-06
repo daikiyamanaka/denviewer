c8cc660a6070e51d764b5c604dd5fb09282b0a31
YutaYamauchi <yamauchi@den.rcast.u-tokyo.ac.jp>
2012-06-06 17:57:06 +0900

    Rename VirtualPan to Translation

---
+++ Translation.cpp
@@ -0,0 +1,66 @@
+#include "Translation.hpp"
+
+Translation::Translation(Model& model, const float radius, const float epsilon ) : _model ( model ), _radius ( radius ), _eps ( epsilon )
+{
+        return;
+}
+
+Translation::~Translation(void)
+{
+    return;
+}
+
+void
+Translation::setSize(const int width, const int height )
+{
+        this->_width = width;
+        this->_height = height;
+}
+
+bool
+Translation::mousePressed ( const MouseEvent* event )
+{
+        if ( !event->isLeftButtonPressed() ) return false;
+        this->setSize ( event->width(), event->height() );
+        this->_oldp = this->project_on_sphere ( event->x(), event->y() );
+        return true;
+}
+
+bool
+Translation::mouseMoved ( const MouseEvent* event )
+{
+        if ( !event->isLeftButtonPressed() ) return false;
+        const Eigen::Vector3f p0 = this->_oldp;
+        const Eigen::Vector3f p1 = this->project_on_sphere ( event->x(), event->y() );
+        this->_oldp = p1;
+        if ( ( p0 - p1 ).norm() < this->_eps ) return false; // do nothing
+        const float cost = p0.dot ( p1 );
+        const float sint = std::sqrt ( 1 - cost * cost );
+        const Eigen::Vector3f axis = p0.cross ( p1 ).normalized();
+        const Eigen::Quaternionf q ( -cost, sint * axis.x(), sint * axis.y(), sint * axis.z() );
+        this->_model.addRotation ( q );
+        return true;
+}
+
+bool
+Translation::mouseReleased ( const MouseEvent* event )
+{
+        event->x();
+        return true;
+}
+
+Eigen::Vector3f
+Translation::project_on_sphere ( const int x, const int y ) const
+{
+        const int cx = this->_width / 2;
+        const int cy = this->_height / 2;
+        const int base = std::min ( cx,cy );
+        const float fx = ( x - cx ) * 1.0 / base / _radius;
+        const float fy = ( y - cy ) * 1.0 / base / _radius;
+
+        Eigen::Vector3f result ( fx, fy, 0 );
+        const float d = fx * fx + fy * fy;
+        if ( d < 1.0f ) result.z() = std::sqrt ( 1.0f - d );
+        result.normalize();
+        return result;
+}
---
+++ Translation.hpp
@@ -0,0 +1,30 @@
+#ifndef VIRTUALPAN_H
+#define VIRTUALPAN_H
+#include "Model.hpp"
+#include "MouseListener.hpp"
+#include <Eigen/Geometry>
+#include <fstream>
+
+class Translation : public MouseListener
+{
+private:
+        Model& _model;
+        int _width;
+        int _height;
+        float _radius;
+        float _eps;
+        std::ofstream fout;
+        Eigen::Vector3f _oldp;
+
+public:
+        Translation(Model& model, const float radius = 0.8, const float epsilon = 1.0e-10);
+        virtual ~VirtualPan();
+        void setSize ( const int width, const int height );
+        virtual bool mousePressed ( const MouseEvent* event );
+        virtual bool mouseMoved ( const MouseEvent* event );
+        virtual bool mouseReleased ( const MouseEvent* event );
+private:
+        Eigen::Vector3f project_on_sphere ( const int x, const int y ) const;
+};
+
+#endif // VIRTUALPAN_H
